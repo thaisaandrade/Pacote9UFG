cf91911d615c0388fad61fead23078bde1119070
marlaaragao <vargas.marla90@gmail.com>
2016-03-01 23:48:06 -0300

    Correcting Issue AVMZvcKu_UoG48Mm2EnM
    On: http://52.37.121.179:9000/issues/search#issues=AVMZvcKu_UoG48Mm2EnM

--- app/src/main/java/inf/ufg/br/muralufg/features/push/RegistrationIntentService.java
+++ app/src/main/java/inf/ufg/br/muralufg/features/push/RegistrationIntentService.java
@@ -58,7 +58,6 @@ public class RegistrationIntentService extends IntentService {
                 // [END get_token]
                 Log.i(TAG, "GCM Registration Token: " + token);
 
-                // TODO: Implement this method to send any registration to your app's servers.
                 sendRegistrationToServer(token);
 
                 // Subscribe to topic channels
