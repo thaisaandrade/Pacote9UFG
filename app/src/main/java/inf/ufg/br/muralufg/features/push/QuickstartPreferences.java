912c3009bb1d6d65ef496a0e0966eba6bc8319c0
marlaaragao <vargas.marla90@gmail.com>
2016-03-02 01:34:19 -0300

    Correcting Issue AVMZvcJt_UoG48Mm2EnL
    On: http://52.37.121.179:9000/issues/search#issues=AVMZvcJt_UoG48Mm2EnL

--- app/src/main/java/inf/ufg/br/muralufg/features/push/QuickstartPreferences.java
+++ app/src/main/java/inf/ufg/br/muralufg/features/push/QuickstartPreferences.java
@@ -21,7 +21,7 @@ public class QuickstartPreferences {
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
     
-    public QuickstartPreferences(){
+    private QuickstartPreferences(){
         //Constructor
     }
 
